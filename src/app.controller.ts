a14993c5ba43437820cc1f102537d3486a6c8bbb
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2024-09-09 20:59:38 +0700

    finish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import axios from 'axios';
 
 @Controller()
 export class AppController {
@@ -12,14 +11,11 @@ export class AppController {
   }
   @Post('sum1')
   async sum1(@Body('numbers') numbers: number[]): Promise<number> {
-    try {
-      const response = await axios.post('http://localhost:3001/math/sum', {
-        numbers,
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error calling microservice', error);
-      throw error;
-    }
+    return this.appService.sum1(numbers);
+  }
+
+  @Post('sum2')
+  async sum2(@Body('numbers') numbers: number[]) {
+    this.appService.sum2(numbers);
   }
 }
